ee77c7307326fdc8a1baea9efd329302d72773cc
r0nd00 <166877929+r0nd00@users.noreply.github.com>
2024-04-14 15:47:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "ra 3arfke a 3chiri hhh 😘";
+  gif.src =
+    "WhatsApp Image 2024-04-14 at 15.38.46_c7d5dce3.jpg";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
